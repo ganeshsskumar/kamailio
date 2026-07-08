42f1937047392a1d300aa56ef10dfa59766644d3
Ganesh kumar <85495411+ganeshsskumar@users.noreply.github.com>
2026-07-08 17:50:50 +0530

    Update license_check_helper.c

--- src/license_check_helper.c
+++ src/license_check_helper.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
-#include "License_Checker.h"
 
-#define TOC_BUFFER_SIZE 30
-#define APP_NAME_BUFFER_SIZE 10
-
-int main(void)
-{
-    INT8 appname[APP_NAME_BUFFER_SIZE] = "IMS";   
-    INT8 toc[TOC_BUFFER_SIZE] = "Fri 2026-02-6 11:00:40 UTC";
-
-    INT32 status = checkLicense(appname, toc);
-    printf("Helper License Check Status: %ld\n", (long)status);
-
-    return (status == LICENSE_VALID) ? 0 : 1;
-}
